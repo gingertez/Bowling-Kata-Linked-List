691d7737f5173a59dd7cf5c8fc05ade118c30550
Terry Robinson <terry@saratez.co.uk>
2017-01-18 20:30:35 +0000

    Added entry point code for running from console

--- BowlingOO/BowlingOO/Program.cs
+++ BowlingOO/BowlingOO/Program.cs
@@ -10,6 +10,16 @@ namespace BowlingLinkedList
     {
         static void Main(string[] args)
         {
+            var game = new BowlingGame(args[0]);
+            try
+            {
+                Console.WriteLine($"Score is {game.Score()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error calculating score: {ex.Message}");
+            }
+            Console.ReadLine();
         }
     }
 }
