1b90ebea6ce31e5641fc1f301fde62b7d6dd90d8
Terry Robinson <terry@saratez.co.uk>
2017-01-18 18:41:56 +0000

    Initial commit - same code as BowlingOO solution ready to convert to use Linked List

---
+++ BowlingOO/BowlingOO/IFrame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingLinkedList
+{
+    public interface IFrame
+    {
+        int FirstRoll { get; }
+        int Score();
+    }
+}
